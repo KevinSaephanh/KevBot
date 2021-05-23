3c4d0dfec5672a8d9b89192f0ec0b20a269b8be7
Kevin Saephanh <kevin.saephanh@gmail.com>
2021-05-22 22:01:34 -0500

    Added validators for messages

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # KevBot
+
+KevBot is a messaging bot for discord that responds to specific messages that match a list item in a query set
+
+NOTE: this app was created following this tutorial: https://realpython.com/how-to-make-a-discord-bot-python/#how-to-make-a-discord-bot-in-the-developer-portal
+
+https://gemisis.medium.com/building-a-serverless-discord-bot-on-aws-5dc7d972c9c6
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import discord
 from dotenv import load_dotenv
 
+import validator
+import constants
+
 # Get env variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -10,12 +13,26 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
-    )
+    print(f'{client.user} is connected to: {guild.name}\n')
+
+
+@client.event
+async def on_message(message):
+    # Check if message was sent from bot
+    if message.author == client.user:
+        return
+
+    # Validate message format
+    content = message.content
+    if validator.is_valid_command(content):
+        response = validator.get_matching_command(content)
+        await message.channel.send(response)
+    else:
+        return
+
 
 client.run(TOKEN)
